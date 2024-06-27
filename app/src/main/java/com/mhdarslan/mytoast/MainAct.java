2e7609ed5e797c59ea3bb80616a5a22513aedb4f
Muhammad Arslan <gery_maz_555@hotmail.com>
2024-06-27 16:10:22 +0500

    updated

--- app/src/main/java/com/mhdarslan/mytoast/MainAct.java
+++ app/src/main/java/com/mhdarslan/mytoast/MainAct.java
@@ -6,12 +6,13 @@ import android.os.Bundle;
 
 import com.mhdarslan.tosty.Toaster;
 
-public class MainActivity extends AppCompatActivity {
+public class MainAct extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.act_main);
+
 
     }
 }
