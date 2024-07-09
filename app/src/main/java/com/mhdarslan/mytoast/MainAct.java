d0431c3146274d5db5add3737e5cd7723dd10b05
Muhammad Arslan <gery_maz_555@hotmail.com>
2024-07-09 20:57:37 +0500

    updated

--- app/src/main/java/com/mhdarslan/mytoast/MainAct.java
+++ app/src/main/java/com/mhdarslan/mytoast/MainAct.java
@@ -1,9 +1,16 @@
 package com.mhdarslan.mytoast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +24,8 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.File;
 
 public class MainAct extends AppCompatActivity {
     Button update_btn;
@@ -93,5 +102,59 @@ public class MainAct extends AppCompatActivity {
         });
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
+    }
+
+
+
+    public void downloadApk(Context context, String url, String subPath) {
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Convert the response to a Uri
+                        Uri uri = Uri.parse(url);
+
+                        // Download the APK
+                        DownloadManager.Request request = new DownloadManager.Request(uri);
+                        request.setTitle("Downloading APK");
+                        request.setDescription("Downloading the updated APK.");
+                        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, subPath +".apk");
+                        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+                        downloadManager.enqueue(request);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Handle error
+                error.printStackTrace();
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+
+    public void installApk(Context context) {
+        String filePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/L3-Craft-MenuBoard-V4.apk";
+        File file = new File(filePath);
+        if (file.exists()) {
+            Uri uri;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                uri = FileProvider.getUriForFile(context, context.getPackageName() + ".provider", file);
+            } else {
+                uri = Uri.fromFile(file);
+            }
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "application/vnd.android.package-archive");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            context.startActivity(intent);
+        }
     }
 }
