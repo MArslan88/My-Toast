7fb1abaa1de28195f66f21311d0d7e4b53d90fb4
Muhammad Arslan <gery_maz_555@hotmail.com>
2024-07-09 18:31:18 +0500

    updated

--- app/src/main/java/com/mhdarslan/mytoast/MainAct.java
+++ app/src/main/java/com/mhdarslan/mytoast/MainAct.java
@@ -3,15 +3,24 @@ package com.mhdarslan.mytoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-import com.mhdarslan.tosty.Toaster;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainAct extends AppCompatActivity {
+    Button update_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
+
+        update_btn = findViewById(R.id.update_btn);
+
+        update_btn.setOnClickListener(view -> {
+            
+        });
+
+
 
 
     }
